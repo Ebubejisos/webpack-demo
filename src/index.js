5314323f8588cc44ce31ce90ac62a77f0f459af5
Ebubejisos <anosikegideon007@gmail.com>
2022-12-11 12:56:39 +0100

    added images and css bundling with webpack.config file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './stones-g6116d4560_1280.jpg';
 
 function component(){
   const element = document.createElement('div');
 
-  // Lodash now imported bby this script
+  // Lodash now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack','newbie'], ' ');
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
